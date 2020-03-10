2e2af1c55be1b03f5c5b5d1c470746676bf23cfb
carmine <carmine.barbato1@gmail.com>
2020-03-10 01:11:48 +0100

    mongodb connection

--- server/meteounpjs/app.py
+++ server/meteounpjs/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 from flask_restplus import Resource, Api
+from flask_pymongo import PyMongo
+from bson.json_util import dumps
 
 app = Flask(__name__, static_folder ='web-app', template_folder ='web-app')
+app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
+mongo = PyMongo(app)
 api = Api(app)
 
 @app.route("/home")
@@ -16,7 +20,12 @@ def get():
 def test():
     return render_template("service-worker.js") """
 
-
+@app.route('/users')
+def users():
+    users = mongo.db.user.find()
+    res = dumps(users)
+    print(res)
+    return res
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0',port=5000)
+    app.run(host = '0.0.0.0',port=5000, debug="true")
